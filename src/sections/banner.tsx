48b5a2f78e838b5f8f6cb4f81fc9efdf675fa01c
Caio Cavalcante <danielcaio75@gmail.com>
2023-02-11 20:20:25 -0300

    Updates the buttons

--- src/sections/banner.tsx
+++ src/sections/banner.tsx
@@ -27,13 +27,13 @@ const Banner = () => {
   return (
     <Section height="60rem" width="100%" color="#010101" fd="column">
       <TextWrapper displaySize={ScreenSizeBool}>
-        <Text displaySize={ScreenSizeBool} animationDelay={0}>
+        <Text displaySize={ScreenSizeBool} animationDelay={"0s"}>
           Transforme.
         </Text>
-        <Text displaySize={ScreenSizeBool} animationDelay={2.5}>
+        <Text displaySize={ScreenSizeBool} animationDelay={"2s"}>
           Otimize.
         </Text>
-        <Text displaySize={ScreenSizeBool} animationDelay={5}>
+        <Text displaySize={ScreenSizeBool} animationDelay={"4s"}>
           Cresça.
         </Text>
         <BrandDescription displaySize={ScreenSizeBool}>
@@ -43,72 +43,60 @@ const Banner = () => {
         </BrandDescription>
       </TextWrapper>
       <ButtonWrapper displaySize={ScreenSizeBool}>
-        <WhiteButton
-          displaySize={ScreenSizeBool} >
-          Clique aqui
-        </WhiteButton>
-        <ColoredButton displaySize={ScreenSizeBool}>Clique aqui</ColoredButton>
+        <WhiteButton displaySize={ScreenSizeBool}>Conheça a Midas</WhiteButton>
+          <ColoredButton displaySize={ScreenSizeBool}>
+            Alavanque seu negócio
+          </ColoredButton>
       </ButtonWrapper>
     </Section>
   );
 };
-const Gradient = keyframes`
-  0% {
-    background-position: 0% 0%;
-  }
-  50% {
-    background-position: 50% 100%;
-  }
-  100% {
-    background-position: 100% 50%;
-  }
-`;
 const TextWrapper = styled.div<{ displaySize: boolean }>`
   width: 100%;
-  height: auto;
+  height: ${({ displaySize = true }) => (displaySize ? "50%" : "85vw")};
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: start;
   align-items: center;
   /* margin-top: ${({ displaySize = true }) =>
     displaySize ? "1rem" : "2rem"}; */
   margin-top: "2rem";
 `;
-const Text = styled.h1<{ displaySize: boolean; animationDelay: number }>`
+const Text = styled.h1<{ displaySize: boolean; animationDelay: string }>`
   width: 100%;
   font-size: ${({ displaySize = true }) => (displaySize ? "7.5rem" : "17vw")};
   text-align: center;
   font-weight: 700;
   height: ${({ displaySize = true }) => (displaySize ? "8.2rem" : "11vh")};
-  animation: gradient 8s ease-in-out infinite;
   background: linear-gradient(
     48deg,
     #ea5308 20%,
     rgba(253, 221, 0, 1) 50%,
     rgba(228, 117, 5, 1) 90%
   );
-  animation-delay: ${({ animationDelay = 0 }) => animationDelay}s;
-  -webkit-text-fill-color: white;
   background-clip: text;
   -webkit-background-clip: text;
 
-  @keyframes gradient {
-    0%,
-    50% {
-        text-shadow: none;
-        -webkit-text-fill-color: white;
+  //animate
+
+  animation: textGradient 6s ${({ animationDelay = 0 }) => animationDelay}
+    ease-in-out infinite;
+  -webkit-text-fill-color: white;
+  @keyframes textGradient {
+    0% {
+      -webkit-text-fill-color: white;
     }
-    80% {
-        -webkit-text-fill-color: transparent;
-        background-position: 0% 100%;
+    50% {
+      -webkit-text-fill-color: white;
+    }
+    60% {
+      -webkit-text-fill-color: transparent;
+      background-position: 0% 100%;
     }
     90% {
-        text-shadow: 0 0 0.4rem #fddd00, 0 0 1.2rem #ea5308;
-        //reduce the opacity of text-shadow
-        -webkit-text-fill-color: rgba(255, 255, 255, 0.7);
+      background-position: 100% 100%;
     }
     100% {
-        text-shadow: none;
       background-position: 100% 0%;
     }
   }
@@ -144,7 +132,7 @@ const WhiteButton = styled.button<{ displaySize: boolean }>`
 
   color: #000;
   border: 0.12rem solid #fff;
-  font-weight: 700;
+  font-weight: 600;
   font-size: 1.1rem;
   background-color: #fff;
   border-radius: 0.5rem;
@@ -155,30 +143,80 @@ const WhiteButton = styled.button<{ displaySize: boolean }>`
     border-color: #fff;
   }
 `;
-const ColoredButton = styled.button<{ displaySize: boolean }>`
-  height: ${({ displaySize = true }) => (displaySize ? "4rem" : "3rem")};
-
-  width: ${({ displaySize = true }) => (displaySize ? "20rem" : "90%")};
-
-  margin: ${({ displaySize = true }) =>
-    displaySize ? "1rem 1rem 1rem 1rem" : "0 0 1rem 0"};
-
-  color: #fff;
-  font-weight: 700;
-  font-size: 1.1rem;
-  border: 0.12rem solid #fff;
+const Shadow = styled.div<{ displaySize: boolean }>`
+  position: relative;
   border-radius: 0.5rem;
-  background-image: linear-gradient(
-    40deg,
-    #ea7908 0%,
-    rgba(253, 221, 0, 1) 40%,
-    rgba(253, 221, 0, 1) 60%,
-    rgba(228, 117, 5, 1) 100%
+
+  background-color: #ff0000;
+  `;
+const ColoredButton = styled.button<{ displaySize: boolean }>`
+height: ${({ displaySize = true }) => (displaySize ? "4rem" : "3rem")};
+
+width: ${({ displaySize = true }) => (displaySize ? "20rem" : "90%")};
+
+margin: ${({ displaySize = true }) =>
+  displaySize ? "1rem 1rem 1rem 1rem" : "0 0 1rem 0"};
+  color: #fff;
+  font-weight: 600;
+  font-size: 1.1rem;
+  border: 1rem solid rgba(255, 0, 0, 0);
+  //make border clear
+
+  box-shadow: 0 0 0.8rem #fddd00, 0 0 0.5rem #000, 0 0 1.3rem #ea5308, 0 0 1.5rem rgba(0, 0, 0, 0.5);
+  //make a big bright shadow like its burning
+  /* box-shadow: 0 0 0.4rem #fddd00, 0 0 1.2rem #ea5308, 0 0 20rem #511e04; */
+  border-radius: 0.5rem;
+  animation: none;
+  background-image: none;
+  background-color: #000;
+  &:hover {
+    border: none;
+    color: #000;
+    //animate gradient
+    background-size: 400% 400%;
+    background-color: #fddd00;
+
+    background-image: conic-gradient(
+      rgba(253, 221, 0, 0.5),
+      rgba(253, 221, 0, 0.5),
+      rgba(228, 117, 5, 0.5),
+      rgba(228, 117, 5, 0.5),
+      rgba(253, 221, 0, 0.5),
+      rgba(228, 117, 5, 0.5),
+      rgba(253, 221, 0, 0.5)
     );
-    -webkit-background-clip: text;
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-size: 80% 70%;
-    animation: ${Gradient} 20s ease-in-out  alternate infinite;
+    animation: gradient 40s ease-in-out alternate infinite;
+    animation: hot 20s forwards;
+  }
+  @keyframes gradient {
+    0% {
+      background-position: 50% 0%;
+    }
+    20% {
+      background-position: 0% 50%;
+    }
+    40% {
+      background-position: 100% 100%;
+    }
+    80% {
+      background-position: 0% 100%;
+    }
+    100% {
+      background-position: 0% 0%;
+    }
+  }
+    @keyframes hot {
+    0% {
+        background-color: #fddd00;
+    }
+    90% {
+    }
+    100% {
+        //make a fire effect with multiple shadows at least 10
+        background-color: #fff;
+        background-image: none;
+        box-shadow: 0 0 1rem yellow, 0 0 1.5rem orange, 0 0 2rem red, 0 0 2.5rem red, 0 0 2rem orange, 0 0 3rem yellow;
+    }
+    }
 `;
 export default Banner;
